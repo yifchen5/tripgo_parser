963c0b86a8486b76e0b7a1e9e2d94eae65f09f75
Joseph Leong <leong.joseph@me.com>
2020-01-17 20:18:31 +1100

    Add all files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='tripgo_parser',
+	version='0.0.1',
+	description='Parser for use with the TripGo API',
+	author='Joseph Leong',
+	author_email='leong.joseph@me.com',
+	packages = find_packages()
+)
+
+   
