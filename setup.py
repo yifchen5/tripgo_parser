dc4995d617ec9a179bd6b72b305f051f99f0be8d
Joseph Leong <leong.joseph@me.com>
2020-01-23 14:13:44 +1100

    updated

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-	name='tripgo_parser',
-	version='0.0.1',
-	description='Parser for use with the TripGo API',
-	author='Joseph Leong',
-	author_email='leong.joseph@me.com',
-	packages = find_packages()
+    name='tripgo_parser',
+    version='0.0.1',
+    description='Parser for use with the TripGo API',
+    url='https://github.com/leongjoseph/tripgo-parser',
+    author='Joseph Leong',
+    author_email='leong.joseph@me.com',
+    license='MIT',
+    packages=['tripgo'],
+    install_requires=['numpy','pandas','requests']  # include all imports
 )
-
-   
