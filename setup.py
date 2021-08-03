ca4a5cde4bc6b89dc1421e39a3638977b9667581
yifchen5 <85542532+yifchen5@users.noreply.github.com>
2021-08-03 21:12:52 +1000

    Update username

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='tripgo_parser',
     version='0.2.0',
     description='Parser for use with the TripGo API',
-    url='https://github.com/leongjoseph/tripgo-parser',
+    url='https://github.com/yifchen5/tripgo-parser',
     author='Joseph Leong',
     author_email='leong.joseph@me.com',
     license='MIT',
